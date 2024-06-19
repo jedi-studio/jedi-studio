d5154a0c0dae53c0563c4f9b6cdbbcdbad72259f
Mark Maher <100785809+Marco5dev@users.noreply.github.com>
2024-06-19 14:05:16 +0300

    Update page.tsx

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,9 +1,11 @@
 const ContactPage = () => {
   return (
-    <div className="ContactPage">
-      <span className="title">Contact</span>
-      <span className="font-bold">Email:</span> support@jedi-studio.com
-      <span className="font-bold">Discord:</span> <a href="https://discord.gg/2JyqPFvSzQ">Server Invite Link</a>
+    <div className="ContactPage flex flex-col justify-center">
+      <h2 className="title">Contact</h2>
+      <div className="flex flex-col gap-2">
+        <span className="font-bold">Email:</span> support@jedi-studio.com
+        <span className="font-bold">Discord:</span> <a href="https://discord.gg/2JyqPFvSzQ">Server Invite Link</a>
+      </div>
     </div>
   );
 };
