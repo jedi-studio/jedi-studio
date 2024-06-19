5fa36e526cab901bb60de1caa86c83664a8076d2
Mark Maher <100785809+Marco5dev@users.noreply.github.com>
2024-06-19 13:56:43 +0300

    Update page.tsx

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -1,7 +1,9 @@
 const ContactPage = () => {
   return (
     <div className="ContactPage">
-      <span className="title">Team Page</span>
+      <span className="title">Contact</span>
+      <span className="font-bold">Email:</span> support@jedi-studio.com
+      <span className="font-bold">Discord:</span> <a href="https://discord.gg/2JyqPFvSzQ">Server Invite Link</a>
     </div>
   );
 };
